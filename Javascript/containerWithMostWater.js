3c594dd356c716dc4385e8f14afbfc62f5bb93c6
esponges <esponges@gmail.com>
2023-08-16 18:40:26 -0600

    max area optimized

--- Javascript/containerWithMostWater.js
+++ Javascript/containerWithMostWater.js
@@ -7,10 +7,13 @@ class MaxArea {
   constructor(){}
 
   maxArea(height) {
-    let maxAIndex = 0;
-    let maxBIndex = 0;
     let maxVol = 0;
+    // [1,8,6,2,5,4,8,3,7]
+    // 1 * 1, 1*8, 1*6...
+    // 8 * 1, 8*8, 8*6...
+    // 6 * 1, 6*8, 6*6...
 
+    console.time('maxArea');
     for(let i = 0; i < height.length; i++) {
       const actual = height[i];
       
@@ -22,11 +25,37 @@ class MaxArea {
 
         if (xVol > maxVol) {
           maxVol = xVol;
-          maxAIndex = i;
-          maxBIndex = j;
         }
       }
     }
+    console.timeEnd('maxArea');
+
+    return maxVol;
+  }
+
+  // leet code solution 
+  maxAreaOptimized(height) {
+    let maxVol = 0;
+    let left = 0;
+    let right = height.length - 1;
+
+    console.time('maxAreaOptimized');
+    while (left < right) {
+      const xDistance = right - left;
+      const lowestHeight = height[left] < height[right] ? height[left] : height[right];
+      const xVol = xDistance * lowestHeight;
+
+      if (xVol > maxVol) {
+        maxVol = xVol;
+      }
+
+      if (height[left] < height[right]) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+    console.timeEnd('maxAreaOptimized');
 
     return maxVol;
   }
