db2d0d92b8e78d1310c4087f8c1582b4c5e825cf
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2023-08-19 17:57:14 -0500

    basic list node

---
+++ Javascript/common.js
@@ -0,0 +1,8 @@
+export class ListNode {
+  next = null;
+  // this.val = val === undefined ? 0 : val;
+  // this.next = next === undefined ? null : next;
+  constructor(val) {
+    this.val = val === undefined ? 0 : val;
+  }
+}
