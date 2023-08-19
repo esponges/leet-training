19cefd7992275ade72fe507c420231506e863cb7
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2023-08-19 18:12:28 -0500

    create list helper

--- Javascript/common.js
+++ Javascript/common.js
@@ -1,8 +1,31 @@
-export class ListNode {
+class ListNode {
   next = null;
   // this.val = val === undefined ? 0 : val;
   // this.next = next === undefined ? null : next;
   constructor(val) {
     this.val = val === undefined ? 0 : val;
   }
+}
+
+/**
+ * @param {ListNode[]} vals
+ */
+
+const createList = (vals) => {
+  const list = new ListNode(vals[0]);
+  let current = list;
+
+  for (let i = 1; i < vals.length; i++) {
+    const node = new ListNode(vals[i]);
+    current.next = node;
+    current = current.next;
+  }
+  console.log('your list', list);
+
+  return list;
+}
+
+module.exports = {
+  ListNode,
+  createList
 }
