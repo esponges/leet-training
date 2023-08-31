b7f639bc65d86211d88c1ef6ef60425fd8f03902
oaps93_surfacehome <oaps93@gmail.com>
2023-08-31 11:13:30 -0600

    Add two numbers list completed in java

--- Java/AddTwoNumbers.java
+++ Java/AddTwoNumbers.java
@@ -1,17 +1,14 @@
-import java.util.List;
-
-import javax.swing.tree.TreeNode;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
 
         AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
         
-        ListNode head1 = new ListNode((int) (Math.random()*10 + 1));
+        ListNode head1 = new ListNode((int) (Math.random()*9 + 1));
         ListNode current1 = head1;
         int nodes1 = 5;
 
-        ListNode head2 = new ListNode((int) (Math.random()*10 + 1));
+        ListNode head2 = new ListNode((int) (Math.random()*9 + 1));
         ListNode current2 = head2;
         int nodes2 = 8;
         
@@ -34,19 +31,27 @@ public class AddTwoNumbers {
         current2 = head2;
 
         //print the constructed linked list
+        System.out.print("List 1: ");
         while(current1 != null){
             System.out.print(current1.val + " ");
             current1 = current1.next;
         }
         System.out.println();
 
+        System.out.print("List 2: ");
          while(current2 != null){
             System.out.print(current2.val + " ");
             current2 = current2.next;
         }
         System.out.println();
 
-        addTwoNumbers.getSum(head2, current2);
+        ListNode sumHead = addTwoNumbers.getSum(head1, head2);
+
+        System.out.print("Result: ");
+        while(sumHead != null){
+            System.out.print(sumHead.val + " ");
+            sumHead = sumHead.next;
+        }
         
     }
 
@@ -56,7 +61,7 @@ public class AddTwoNumbers {
         ListNode currentL2 = l2;
 
         ListNode newSumList = new ListNode(0);
-        ListNode nextNewSumList = newSumList.next;
+        ListNode currentNewSumList = newSumList;
 
         int carry = 0;
 
@@ -70,10 +75,13 @@ public class AddTwoNumbers {
             } else if( sum == 10){
                 carry = 1;
                 sum = 0;
-            } 
+            } else {
+                carry = 0;
+            }
+            
            
-            nextNewSumList = new ListNode(sum);
-            nextNewSumList = nextNewSumList.next;
+            currentNewSumList.next = new ListNode(sum);
+            currentNewSumList = currentNewSumList.next;
             
             currentL1 = currentL1.next;
             currentL2 = currentL2.next;
@@ -90,10 +98,13 @@ public class AddTwoNumbers {
             } else if( sum == 10){
                 carry = 1;
                 sum = 0;
-            } 
+            } else {
+                carry = 0;
+            }
+            
 
-            nextNewSumList = new ListNode(sum);
-            nextNewSumList = nextNewSumList.next;
+            currentNewSumList.next = new ListNode(sum);
+            currentNewSumList = currentNewSumList.next;
 
             currentL1 = currentL1.next;
 
@@ -109,10 +120,14 @@ public class AddTwoNumbers {
             } else if( sum == 10){
                 carry = 1;
                 sum = 0;
-            } 
+            } else {
+                carry = 0;
+            }
 
-            nextNewSumList = new ListNode(sum);
-            nextNewSumList = nextNewSumList.next;
+            
+
+            currentNewSumList.next = new ListNode(sum);
+            currentNewSumList = currentNewSumList.next;
 
             currentL2 = currentL2.next;
 
@@ -120,7 +135,7 @@ public class AddTwoNumbers {
 
         if(carry > 0){
 
-            nextNewSumList = new ListNode(carry);
+            currentNewSumList.next = new ListNode(carry);
 
         }
 
