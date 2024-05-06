29f010d2794232ed36cb6220e526ee6f0ad9a013
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-05-06 11:05:43 -0600

    base case

---
+++ smallestString.js
@@ -0,0 +1,112 @@
+/* 2734. Lexicographically Smallest String After Substring Operation
+Medium
+Topics
+Companies
+Hint
+You are given a string s consisting of only lowercase English letters. In one operation, you can do the following:
+
+Select any non-empty substring of s, possibly the entire string, then replace each one of its characters with the previous character of the English alphabet. For example, 'b' is converted to 'a', and 'a' is converted to 'z'.
+Return the lexicographically smallest string you can obtain after performing the above operation exactly once.
+
+A substring is a contiguous sequence of characters in a string.
+
+A string x is lexicographically smaller than a string y of the same length if x[i] comes before y[i] in alphabetic order for the first position i such that x[i] != y[i].
+ 
+
+Example 1:
+
+Input: s = "cbabc"
+Output: "baabc"
+Explanation: We apply the operation on the substring starting at index 0, and ending at index 1 inclusive. 
+It can be proven that the resulting string is the lexicographically smallest. 
+Example 2:
+
+Input: s = "acbbc"
+Output: "abaab"
+Explanation: We apply the operation on the substring starting at index 1, and ending at index 4 inclusive. 
+It can be proven that the resulting string is the lexicographically smallest. 
+Example 3:
+
+Input: s = "leetcode"
+Output: "kddsbncd"
+Explanation: We apply the operation on the entire string. 
+It can be proven that the resulting string is the lexicographically smallest. 
+ 
+
+Constraints:
+
+1 <= s.length <= 3 * 105
+s consists of lowercase English letters
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+25.8K
+Submissions
+85K
+Acceptance Rate
+30.3% 
+
+https://leetcode.com/problems/lexicographically-smallest-string-after-substring-operation/description/
+*/
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+const alphabet = {
+  a: "a",
+  b: "a",
+  c: "b",
+  d: "c",
+  e: "d",
+  f: "e",
+  g: "f",
+  h: "g",
+  i: "h",
+  j: "i",
+  k: "j",
+  l: "k",
+  m: "l",
+  n: "m",
+  o: "n",
+  p: "o",
+  q: "p",
+  r: "q",
+  s: "r",
+  t: "s",
+  w: "t",
+  x: "w",
+  y: "x",
+  z: "y"
+}
+
+var smallestString = function (s) {
+  let firstA = s[0] === 'a' ? 0 : undefined;
+
+  const letters = s.split('');
+  for (let i = 0; i < letters.length; i++) {
+      const actual = letters[i];
+      // found first A no need to keep changing next letters
+      if (actual === 'a' && firstA === undefined){
+          break;
+      }
+      // keep swaping letters
+      letters[i] = alphabet[actual];
+  }
+
+  return letters.join('');
+};
+
+const cases = [
+  "cbabc",
+  "acbbc",
+  "leetcode"
+];
+
+cases.forEach(c => {
+  console.log(smallestString(c));
+});
