08b802dd33cfcd050d9ebd855e13fd445fb0cf00
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-05-20 19:30:00 -0600

    ideal solutions

--- rob.js
+++ rob.js
@@ -44,44 +44,91 @@ https://leetcode.com/problems/house-robber/description/
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
+var rob = function (nums) {
   function traverse(arr) {
-      if (arr.length <= 1) return arr[0] || 0;
-      
-      let maxVal = 0;
-      const occs = {};
+    if (arr.length <= 1) return arr[0] || 0;
 
-      for (let i = 0; i < arr.length; i++) {
-          const actual = arr[i];
+    let maxVal = 0;
+    const occs = {};
 
-          if (!occs[actual]) occs[actual] = [i];
-          else occs[actual].push(i);
+    for (let i = 0; i < arr.length; i++) {
+      const actual = arr[i];
 
-          if (actual > maxVal) maxVal = actual;
-      }
+      if (!occs[actual]) occs[actual] = [i];
+      else occs[actual].push(i);
 
-      if (maxVal === 0 || nums.length <= 2) return 0;
+      if (actual > maxVal) maxVal = actual;
+    }
 
-      const sums = [];
+    if (maxVal === 0 || nums.length <= 2) return 0;
 
-      for (repIdx of occs[maxVal]) {
-          const left = repIdx > 1 ? arr.slice(0, repIdx - 1) : [];
-          const right = repIdx > arr.length - 2 ? arr.slice(repIdx + 2): [];
+    const sums = [];
 
-          const res = maxVal + traverse(left) + traverse(right);
-          sums.push(res);
-      }
+    for (repIdx of occs[maxVal]) {
+      const left = repIdx > 1 ? arr.slice(0, repIdx - 1) : [];
+      const right = repIdx < arr.length - 2 ? arr.slice(repIdx + 2) : [];
 
-      return Math.max(...sums);
+      const res = maxVal + traverse(left) + traverse(right);
+      sums.push(res);
+    }
+
+    return Math.max(...sums);
   }
 
   return traverse(nums);
 };
 
+function easiestToUnderstandSol(nums) {
+  function rob(arr, i) {
+    if (i > arr.length - 1) return 0;
+
+    // we have two options everytime we want to rob
+    // 1
+    // left side
+    // we can either rob this house and get the value of 2 houses ahead
+    // 2
+    // right side
+    // rob the next house but not this house
+    // pick the MAX value from that
+    // we're done once I exceeds the length (no more house to rob)
+
+    return Math.max(rob(arr, i + 2) + arr[i], rob(arr, i + 1));
+  }
+
+  return rob(nums, 0);
+}
+
+function solWithMemo(nums) {
+  // the problem with the above solution is
+  // that it a n^2 time complexity due to the
+  // Math.Max recursive calls
+
+  // try to not overflow the stack with a memo
+  const memo = Array(nums.length).fill(null);
+
+  function rob(arr, i) {
+    if (i > arr.length - 1) return 0;
+
+    if (memo[i]) return memo[i];
+
+    const res = Math.max(rob(arr, i + 2) + arr[i], rob(arr, i + 1));
+    memo[i] = res;
+
+    return res;
+  }
+
+  return rob(nums, 0);
+}
+
 const cases = [
-  [1,1,1]
-  // [1,2,3,1],
+  // [2, 3, 2], // prev implementation will fail for this since
+  // always look for the first highest element... :'()
   // [2,7,9,3,1],
+  // [1,1,1]
+  // [1,2,3,1],
+  [2,7,9,3,1],
 ];
 
-cases.forEach(c => console.log(rob(c))); 
+// cases.forEach(c => console.log(rob(c)));
+cases.forEach(c => console.log(easiestToUnderstandSol(c)));
+cases.forEach((c) => console.log(solWithMemo(c)));
