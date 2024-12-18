1a2bb9ffc3832ff48c9902cfcfbfca028399aedc
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-12-18 17:06:45 -0600

    improved runtime and memory

--- Javascript/majorityElement.js
+++ Javascript/majorityElement.js
@@ -2,8 +2,6 @@
 
 // The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
- 
-
 // Example 1:
 
 // Input: nums = [3,2,3]
@@ -12,14 +10,12 @@
 
 // Input: nums = [2,2,1,1,1,2,2]
 // Output: 2
- 
 
 // Constraints:
 
 // n == nums.length
 // 1 <= n <= 5 * 104
 // -109 <= nums[i] <= 109
- 
 
 // Follow-up: Could you solve the problem in linear time and in O(1) space?
 
@@ -34,11 +30,11 @@ class Majority {
     const reps = new Map();
 
     // create repetition map
-    for(let i = 0; i < this.nums.length; i++) {
+    for (let i = 0; i < this.nums.length; i++) {
       const actual = this.nums[i];
 
       const existingValue = reps.get(actual);
-      if (existingValue){
+      if (existingValue) {
         reps.set(actual, existingValue + 1);
       } else {
         reps.set(actual, 1);
@@ -53,7 +49,7 @@ class Majority {
         elementCount = value;
         element = key;
       }
-    })
+    });
 
     return element;
   }
@@ -68,7 +64,6 @@ console.log(m2.getMajorityElement());
 const m3 = new Majority([1, 2, 5, 5, 6, 6, 1, 3, 3, 2, 3, 6, 6]);
 console.log(m3.getMajorityElement());
 
-
 /// nov 24
 
 // accepted but bad runtime and memory
@@ -77,19 +72,16 @@ console.log(m3.getMajorityElement());
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
   const mem = {};
-
-  for (n of nums) {
-      if (!mem[n]) mem[n] = 1;
-      else mem[n] = mem[n] + 1;
-  }
-
   let max = [0, 0];
 
-  Object.entries(mem).forEach(([key, val]) => {
-      if (val > max[1]) max = [parseInt(key), val];
-  });
+  for (n of nums) {
+    if (!mem[n]) mem[n] = 1;
+    else mem[n] = mem[n] + 1;
+
+    if (mem[n] > max[1]) max = [n, mem[n]];
+  }
 
   return max[0];
 };
