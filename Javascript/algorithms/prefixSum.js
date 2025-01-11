e6d68cb2f2671754aa62fa2665d6fd79beaacb21
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2025-01-11 15:53:08 -0600

    medium prefix sum

--- Javascript/algorithms/prefixSum.js
+++ Javascript/algorithms/prefixSum.js
@@ -77,6 +77,8 @@ Acceptance Rate
 
 */
 
+// solved >80% runtime
+
 var els = [];
 
 /**
@@ -112,3 +114,83 @@ NumArray.prototype.sumRange = function(left, right) {
 
 // ["NumArray","sumRange","sumRange","sumRange"]
 // [[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]
+
+
+/* 
+  974. Subarray Sums Divisible by K
+Attempted
+Medium
+Topics
+Companies
+Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+
+A subarray is a contiguous part of an array.
+
+ 
+
+Example 1:
+
+Input: nums = [4,5,0,-2,-3,1], k = 5
+Output: 7
+Explanation: There are 7 subarrays with a sum divisible by k = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+Example 2:
+
+Input: nums = [5], k = 9
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-104 <= nums[i] <= 104
+2 <= k <= 104
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+381.6K
+Submissions
+687.5K
+Acceptance Rate
+55.5%
+
+https://leetcode.com/problems/subarray-sums-divisible-by-k/description/
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraysDivByK = function(nums, k) {
+  const sums = [];
+
+  for (let i = 0; i < nums.length; i++) {
+      if (i == 0) sums.push(nums[i]);
+      else sums.push(nums[i] + sums[i - 1]);
+  }
+
+  let count = 0, left = 0, right = nums.length - 1;
+  while (left <= right) {
+      const sum = left > 0 ? sums[right] - sums[left - 1] : sums[right];
+      if (sum % k == 0) count++;
+
+      right--;
+      if (right >= left) continue;
+
+      // move pointers
+      left++;
+      right = nums.length - 1;
+  }
+
+  return count;
+};
+
+const cases = [
+  [[4,5,0,-2,-3,1], 5],
+  [[5], 9]
+];
+
+// TODO: Fix TLE due to the pointers
